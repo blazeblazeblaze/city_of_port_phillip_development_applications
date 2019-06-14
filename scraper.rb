f1c57269a0a714b87d88b973c6a7ec8acf936051
Blaze Hadzik <hadzikblazej@gmail.com>
2019-06-14 12:23:00 +1000

    init commit

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,47 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+# frozen_string_literal: true
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+require 'mechanize'
+require 'scraperwiki'
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+HEADERS = {
+  'HTTP_USER_AGENT': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.13) Gecko/2009073022 Firefox/3.0.13',
+  'HTTP_ACCEPT': 'text/html,application/xhtml+xml,application/xml; q=0.9,*/*; q=0.8',
+  'Content-Type': 'application/x-www-form-urlencoded'
+}.freeze
+
+BASE_URL = 'https://eservices.portphillip.vic.gov.au/ePathway/Production/Web/GeneralEnquiry'
+HOMEPAGE_URL = BASE_URL + '/EnquiryLists.aspx'
+RESULTS_URL = BASE_URL + '/EnquirySummaryView.aspx'
+
+agent = Mechanize.new
+
+# 1. Create session
+sessionpage = agent.get(HOMEPAGE_URL, headers: HEADERS)
+sessionpage.forms.first.radiobuttons[1].check # Select all applications
+agent.submit(sessionpage.forms.first, sessionpage.forms.first.buttons.last) # Submit form
+
+# 2. Retrieve number of pages to be scraped
+pagination_page = agent.get(RESULTS_URL)
+pagination_text = pagination_page.at('#ctl00_MainBodyContent_mPagingControl_pageNumberLabel').inner_text.strip
+pages_count = pagination_text.scan(/\d+/).last.to_i
+
+# 3. Scraping starts here
+(1..pages_count).each do |page_number|
+  page = agent.get("#{RESULTS_URL}?PageNumber=#{page_number}")
+  table_rows = page.at('table.ContentPanel').search('tr.ContentPanel', 'tr.AlternateContentPanel')
+
+  table_rows.each do |row|
+    raw_data = row.search('td')
+
+    record = {
+      council_reference: raw_data[0].inner_text.strip,
+      address: raw_data[2].inner_text.strip,
+      description: raw_data[1].inner_text.strip,
+      info_url: HOMEPAGE_URL,
+      date_scraped: Date.today.to_s,
+      date_received: Date.parse(raw_data[3].inner_text.strip).to_s
+    }
+
+    ScraperWiki.save_sqlite([:council_reference], record)
+  end
+end
